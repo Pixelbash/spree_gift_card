222ef470e6435ac58f39ce151ae295ecf5a0b187
Joe S <joe@pixelbash.co.nz>
2017-10-23 20:01:53 -0400

    Create source_payments

--- app/assets/javascripts/spree/backend/spree_gift_card.js
+++ app/assets/javascripts/spree/backend/spree_gift_card.js
@@ -1,1 +1,26 @@
 //= require spree/backend
+
+
+SpreeGiftCard = {
+  hideSettings: function(paymentMethod) {
+    if (SpreeGiftCard.paymentMethodID && paymentMethod.val() == SpreeGiftCard.paymentMethodID) {
+      $('.payment-method-settings').children().hide();
+      $('#payment_amount').prop('disabled', 'disabled');
+      $('button[type="submit"]').prop('disabled', 'disabled');
+      $('#paypal-warning').show();
+    } else if (SpreeGiftCard.paymentMethodID) {
+      $('.payment-method-settings').children().show();
+      $('button[type=submit]').prop('disabled', '');
+      $('#payment_amount').prop('disabled', '')
+      $('#paypal-warning').hide();
+    }
+  }
+}
+
+$(document).ready(function() {
+  checkedPaymentMethod = $('[data-hook="payment_method_field"] input[type="radio"]:checked');
+  SpreeGiftCard.hideSettings(checkedPaymentMethod);
+  paymentMethods = $('[data-hook="payment_method_field"] input[type="radio"]').click(function (e) {
+    SpreeGiftCard.hideSettings($(e.target));
+  });
+})
